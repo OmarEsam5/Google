75380b50d2b6752e108a82ec86880e58c983ff6f
omaressam <oesam094@gmail.com>
2022-09-19 23:26:56 +0200

    changes in icon bar on js and css and html fils

--- doc.js
+++ doc.js
@@ -1,29 +1,52 @@
 // clic th bar
 
 let i = document.getElementById("i");
-let ul = document.getElementById("ul");
+let ulLinks = document.getElementById("ul");
+let containerLinks = document.querySelector(".links-bar")
 
-i.addEventListener("click", clic)
+
+i.onclick = () => {
+
+    ulLinks.classList.toggle("open")
+
+    i.classList.toggle("back")
+
+}
 
 document.onclick = (e) => {
-    if(e.target.id != 'i') {
-        ul.style.display = "none";
-        i.style.backgroundColor = ""
-    }else {
-        ul.style.display === "block"
+
+    if(e.target !== i && e.target !== containerLinks) {
+
+        ulLinks.classList.remove("open")
+
+        i.classList.remove("back")
+
     }
 }
 
+// who was before
 
-function clic() {
-    if (ul.style.display === "none") {
-        ul.style.display = "block";
-        i.style.backgroundColor = "rgba(60,64,67,.08)"
-    }else {
-        ul.style.display = "none";
-        i.style.backgroundColor = ""
-    }
-}
+// i.addEventListener("click", clic)
+
+// document.onclick = (e) => {
+//     if(e.target.id != 'i') {
+//         ul.style.display = "none";
+//         i.style.backgroundColor = ""
+//     }else {
+//         ul.style.display === "block"
+//     }
+// }
+
+
+// function clic() {
+//     if (ul.style.display === "none") {
+//         ul.style.display = "block";
+//         i.style.backgroundColor = "rgba(60,64,67,.08)"
+//     }else {
+//         ul.style.display = "none";
+//         i.style.backgroundColor = ""
+//     }
+// }
 
 // the mainuo
 
